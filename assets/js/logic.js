3abb6908a146072098b5b816e074240432e86099
DavouJ <djjobbi@hotmailcom>
2024-01-08 20:48:05 +0000

    adding event listeners for saving tasks

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,12 @@
-const userinput = $('.user-input')
+const userInput = $('textarea')
+
+const userTasks = []
 $(document).ready(function(){
     function displayTime(){
         $('#currentDay').text(dayjs().format('DD MMM YYYY[,] hh:mm:ss a'))
     }
 
     function setCurrent(){
-        
         let timeBlock = $('.time-block')
         timeBlock.each(function(){
             
@@ -23,7 +24,14 @@ $(document).ready(function(){
         })
     }
 
+    $('button').on("click", function(){
+        let parent = $(this).parent()
+        
+        userTasks[parseInt($(this).parent().attr('id'))] = parent.find(userInput).val()
+        console.log(userTasks)
+        userTasksSerialised = JSON.stringify(userTasks)
 
+    })
 
     setInterval(displayTime,1000)
     setCurrent()
